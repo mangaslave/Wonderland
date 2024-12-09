7b7ed3ebdfb2c72499390510357e93857535680c
adosha o <adoshao@adoshas-Air.home>
2024-12-08 23:46:26 -0800

    protect routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ClerkProvider } from "@clerk/clerk-react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import App from "./App";
 import Dashboard from "./pages/Dashboard";
 import "./App.css";
@@ -22,20 +22,56 @@ createRoot(document.getElementById("root")!).render(
       <Router>
         <UserSignupSync />
         <Routes>
+          {/* Public Routes */}
           <Route path="/" element={<App />} />
+          <Route path="/signin" element={<App />} />
+          <Route path="/signup" element={<App />} />
+          
+          {/* Protected Routes */}
           <Route
-          path="/dashboard"
-          element={
-              <Dashboard />
-          }
-        />
-          <Route path="/saved" element={<WatchlistPage />} />
-          <Route path="/account" element={<AccountPage />} />
-          <Route path="/signin" element={<App />} /> 
-          <Route path="/signup" element={<App />} /> 
-          <Route path="/movie/:movieId" element={<MovieDetailPage />} />
+            path="/dashboard"
+            element={
+              <SignedIn>
+                <Dashboard />
+              </SignedIn>
+            }
+          />
+          <Route
+            path="/saved"
+            element={
+              <SignedIn>
+                <WatchlistPage />
+              </SignedIn>
+            }
+          />
+          <Route
+            path="/account"
+            element={
+              <SignedIn>
+                <AccountPage />
+              </SignedIn>
+            }
+          />
+          <Route
+            path="/movie/:movieId"
+            element={
+              <SignedIn>
+                <MovieDetailPage />
+              </SignedIn>
+            }
+          />
+
+          <Route
+            path="*"
+            element={
+              <SignedOut>
+                <Navigate to="/signin" />
+              </SignedOut>
+            }
+          />
         </Routes>
       </Router>
     </ClerkProvider>
   </StrictMode>
 );
+
